3e4a09797488704303bd06251b7f49b54259e1ef
EricK-00 <carter000@naver.com>
2022-12-22 17:51:38 +0900

    c

--- CppProgram/CppProgram.cpp
+++ CppProgram/CppProgram.cpp
@@ -1,83 +1,155 @@
 ﻿#include <iostream>
 #include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
 
-char board[1024][1024];
-int N, maxRow, maxCol;
+std::vector<int> adj[1001], r[2];
+bool v[2][1001];
+//int array[5];
 
-int PowerOf2(int power)
+enum class typeNum
 {
-    if (power == 1)
-        return 2;
+    dfs = 0,
+    bfs = 1
+};
 
-    return 2 * PowerOf2(power - 1);
+void PrintDFS()
+{
+        for (int j : r[0])
+            std::cout << j << ' ';
+        std::cout << '\n';
 }
 
-void DrawStar(int n, int mid)
+void PrintBFS()
 {
-    if (n == 0) return;
+    for (int j : r[0])
+        std::cout << j << ' ';
+    std::cout << '\n';
+}
 
-    int row = PowerOf2(n) - 1;
-    //int rowEnd = row - 1;
-
-    std::cout << "row: " << row << ", mid " << mid << std::endl;
-
-    if (n % 2 == 0)
+void SetAdjVector(int nodeCount, int edgeCount, std::vector<int>* adjVector)
+{
+    for (int i = 0; i < edgeCount; i++)
     {
-        for (int i = row - 1; i > 0; i--)
-        {
-            board[i + mid][maxCol / 2 + i] = '*';
-            board[i + mid][maxCol / 2 - i] = '*';
-        }
-        for (int i = 0; i <= maxCol / 2; i++)
-        {
-            board[mid][maxCol / 2 + i] = '*';
-            board[mid][maxCol / 2 - i] = '*';
-        }
-    }
-    else
-    {
-        //std::cout << "rS: " << rowStart << ", rE " << rowEnd << std::endl;
-        for (int i = 0; i < row; i++)
-        {
-            board[i + mid][maxCol / 2 + i] = '*';
-            board[i + mid][maxCol / 2 - i] = '*';
-        }
-        for (int i = 0; i <= maxCol / 2; i++)
-        {
-            board[row + mid][maxCol / 2 + i] = '*';
-            board[row + mid][maxCol / 2 - i] = '*';
-        }
+        int e1, e2;
+        std::cin >> e1 >> e2;
+        adjVector[e1].push_back(e2); adjVector[e2].push_back(e1);
     }
 
-    DrawStar(n - 1, (row + 1) / 2);
+    for (int i = 1; i <= nodeCount; i++)
+        std::sort(adjVector[i].begin(), adjVector[i].end());
+}
+
+void DFS(int V)
+{
+    std::stack<int> stk;
+    stk.push(V);
+    r[0].push_back(V);
+    while (!stk.empty())
+    {
+        bool pop = true;
+        int nd = stk.top();
+        v[0][nd] = true;
+
+        for (int nxt : adj[nd])
+            if (!v[nxt])
+            {
+                v[0][nxt] = true;
+                stk.push(nxt);
+                pop = false;
+                r[0].push_back(nxt);
+                break;
+            }
+        if (pop) stk.pop();
+    }
+}
+
+void BFS(int V)
+{
+    std::queue<int> q;
+    q.push(V);
+    while (!q.empty())
+    {
+        int nd = q.front();
+        q.pop();
+        v[1][nd] = true;
+        r[1].push_back(nd);
+
+        for (int nxt : adj[nd])
+            if (!v[nxt])
+            {
+                v[1][nxt] = true;
+                q.push(nxt);
+            }
+    }
+}
+
+void Merge(int array[], int low, int mid, int high)
+{
+    int l = low;
+    int r = mid + 1;
+    int idx = 0;
+
+    int* temp = new int[high - low + 1];
+
+    while (l < mid + 1 && r < high + 1)
+    {
+        if (array[low] > array[high])
+            temp[idx++] = array[r++];
+        else
+            temp[idx++] = array[l++];
+    }
+
+    for (int i = l; l < mid + 1; l++)
+    {
+        temp[idx++] = array[i];
+    }
+    for (int i = r; r < high + 1; r++)
+    {
+        temp[idx++] = array[i];
+    }
+
+    for (int i = 0; i < idx; i++)
+    {
+        array[low + i] = temp[i];
+    }
+}
+
+void MergeSort(int array[], int low, int high)
+{
+    if (low == high) return;
+    int mid = high / 2;
+
+    MergeSort(array, low, mid);
+    MergeSort(array, mid + 1, high);
+    Merge(array, low, mid, high);
 }
 
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
+    std::ios_base::sync_with_stdio(false); std::cin.tie(NULL);
 
-    std::cin >> N;
-
-    maxCol = PowerOf2(N + 1) - 3;
-
-    for (int i = 0; i < maxRow; i++)
+    size_t arraySize = 5;
+    int* array = new int[arraySize];
+    //size_t arraySize = _msize(array) / sizeof(*array);
+    for (int i = 0; i < arraySize; i++)
     {
-        for (int j = 0; j < maxCol; j++)
-        {
-            board[i][j] = ' ';
-        }
+        std::cout << "입력(" << i << "): ";
+        std::cin >> array[i];
+        std::cout << '\n' << array[i];
     }
 
-    DrawStar(N, 0);
+    MergeSort(array, 0, arraySize - 1);
 
-    for (int i = 0; i < maxRow; i++)
-    {
-        for (int j = 0; j < maxCol; j++)
-        {
-            std::cout << board[i][j];
-        }
-        std::cout << '\n';
-    }
+    //int N, M, V;
+    //std::cin >> N >> M >> V;
+
+    //SetAdjVector(N, M, adj);
+    //DFS(V);
+    //BFS(V);
+    //PrintDFS();
+    //PrintBFS();
+
+    return 0;
 }
